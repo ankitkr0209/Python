457d45ad1e04b5ef2c14fa50c76529d435c64a90
ankitkr0209 <ankit.kumar02sep@gmail.com>
2024-09-08 22:57:40 +0530

    numbers

---
+++ basic/number.py
@@ -0,0 +1,9 @@
+import math
+# floor give the the value same as greatest integer function 
+
+math.floor(3.7)
+math.floor(-3.4)
+
+# trunc give the value closest to zero(0) value
+math.trunc(3.7)
+math.trunc(-3.9)
