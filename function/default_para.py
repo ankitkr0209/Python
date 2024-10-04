02466ea315f662c57a87206772979a9f8586f5bc
ankitkr0209 <ankit.kumar02sep@gmail.com>
2024-10-04 23:28:29 +0530

    function

---
+++ function/default_para.py
@@ -0,0 +1,8 @@
+
+
+def greet(name = "Bhaisaab"):
+    return "Hello, " + name + " !"
+
+
+print(greet("rani"))
+print(greet())
---
+++ function/lambda_fun.py
@@ -0,0 +1,8 @@
+
+
+
+cube = lambda x: x**3
+
+num = int(input("Enter the number you want a cube :  "))
+
+print(cube(num))
